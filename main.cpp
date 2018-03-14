4e89b27cd54865e28d64619580dce682b360d066
Mitchell <jmitchell3610@flpoly.org>
2018-03-14 18:54:50 -0400

    updated for parameter search

--- main.cpp
+++ main.cpp
@@ -13,15 +13,27 @@ void update_satellite(satellite_t *sat);
 
 int main(int argc, char **argv) {
 	int numthreads = omp_get_num_threads();
+	// Defaults with no params
+	int totalItter = 300 * 60; // total seconds to simulate TODO set this with cmd line arg as total minutes
+	int numberSats = 2; // some way of loading our satilite orbit params from a config-like file (e.g. json, txt) will be needed
 
     #pragma omp parallel num_threads(5)
     {
         printf("Hello from %i of %i\n", omp_get_thread_num(), omp_get_num_threads());
     }
 
+	for (int i = 0; i < argc; i++) {
+		if (argv[i] == "-time" || argv[i] == "-t") {
+			// Set time in minutes
+			totalItter = (int)argv[i + 1] * 60; 
+		}
+		if (argv[i] == "-sats" || argv[i] == "-s") {
+			// Set time in minutes
+			numberSats = (int)argv[i + 1];
+		}
+	}
+
 	int curItter = 0;
-	int totalItter = 300 * 60; // total seconds to simulate TODO set this with cmd line arg as total minutes
-	int numberSats = 2; // some way of loading our satilite orbit params from a config-like file (e.g. json, txt) will be needed
 
 	satellite_t *satellites = (satellite_t*) malloc( numberSats * sizeof(satellite_t) );
 	init_satellites(satellites, numberSats);
