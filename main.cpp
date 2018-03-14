f02e574cee6c3a7897f884419cc81da9455abdc7
Keely Hill <KeelyHill@gmx.com>
2018-03-14 13:47:07 -0400

    Cleaned up and added some boilerplate stuff

--- main.cpp
+++ main.cpp
@@ -5,35 +5,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-
-// #include "omp.h"
+#include "omp.h"
 
 #include "common.hpp"
 
 void update_satellite(satellite_t *sat);
 
 int main(int argc, char **argv) {
-	// int numthreads = omp_get_num_threads();
+	int numthreads = omp_get_num_threads();
 
-	// printf("Hello. #: %i", numthreads);
+	printf("# Threads: %i\n", numthreads);
 
-	// #pragma omp parallel {
-  		printf("Hello world!\n");
-	// }
+    #pragma omp parallel num_threads(5)
+    {
+        printf("Hello from %i of %i\n", omp_get_thread_num(), omp_get_num_threads());
+    }
 
-
-	int totalItter = 50 * 60; // total seconds to simulate TODO set this with cmd line arg as total minutes
-	int numberSats = 1;
+	int curItter = 0;
+	int totalItter = 300 * 60; // total seconds to simulate TODO set this with cmd line arg as total minutes
+	int numberSats = 2; // some way of loading our satilite orbit params from a config-like file (e.g. json, txt) will be needed
 
 	satellite_t *satellites = (satellite_t*) malloc( numberSats * sizeof(satellite_t) );
 	init_satellites(satellites, numberSats);
 
+	for (int curItter = 0; curItter<totalItter; curItter++) {
 
-	for (int i = 0; i<totalItter; i++) {
+		// printf("%f\n", satellites[0].trueAnomaly);
+		// printf("%f\n", satellites[1].trueAnomaly);
 
-		printf("%f\n", satellites[0].trueAnomaly);
-
-		update_satellite(&satellites[0]);
+		for (int i=0; i<numberSats; i++)
+			update_satellite(&satellites[i]);
 	}
 
 
@@ -47,5 +48,10 @@ void update_satellite(satellite_t *sat) {
 
 /* TODO Append new info of satellites to output file format. */
 void logStep(FILE *f, satellite_t *satellites) {
+
+	// 'cur time' should be logged
+
+	// Crital anamoly is the only thing that changes in relation to the spacecrafts orbit
+	// (i.e. dont write all the other stuff unless it changes tick-to-tick)
 
 }
