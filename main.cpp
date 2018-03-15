134b84d8ceecd2997b4d56b9057be540909162a4
Mitchell <jmitchell3610@flpoly.org>
2018-03-14 20:49:20 -0400

    updated prams loop

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <math.h>
 #include "omp.h"
 
@@ -12,25 +13,29 @@
 void update_satellite(satellite_t *sat);
 
 int main(int argc, char **argv) {
-	int numthreads = omp_get_num_threads();
+	/*int numthreads = omp_get_num_threads();*/
 	// Defaults with no params
 	int totalItter = 300 * 60; // total seconds to simulate TODO set this with cmd line arg as total minutes
 	int numberSats = 2; // some way of loading our satilite orbit params from a config-like file (e.g. json, txt) will be needed
-
+	/*
     #pragma omp parallel num_threads(5)
     {
         printf("Hello from %i of %i\n", omp_get_thread_num(), omp_get_num_threads());
     }
-
-	for (int i = 0; i < argc; i++) {
-		if (argv[i] == "-time" || argv[i] == "-t") {
+	*/
+	for (int i = 1; i < argc; i++) {
+		if (strcmp(argv[i], "-sats") == 0 || strcmp(argv[i], "-s") == 0) {
 			// Set time in minutes
-			totalItter = (int)argv[i + 1] * 60; 
+			char* numberSats = argv[i + 1];
+			printf("numberSats = %s\n", numberSats);
 		}
-		if (argv[i] == "-sats" || argv[i] == "-s") {
+		else if (strcmp(argv[i], "-time") == 0 || strcmp(argv[i], "-t") == 0) {
 			// Set time in minutes
-			numberSats = (int)argv[i + 1];
+			char* totalIterStr = argv[i + 1];
+			int totalIterNum = atoi(totalIterStr) * 60;
+			printf("totalItter = %i\n ", totalIterNum);			
 		}
+		
 	}
 
 	int curItter = 0;
