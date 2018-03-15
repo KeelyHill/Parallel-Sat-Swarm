f655a9fc73ca539d647eaa24e4d17d8ae8b5af8d
Keely Hill <KeelyHill@gmx.com>
2018-03-14 21:14:19 -0400

    Actually set real totalItter var based on flag, adjusted some TODOs and added help flag.

--- main.cpp
+++ main.cpp
@@ -13,37 +13,45 @@
 void update_satellite(satellite_t *sat);
 
 int main(int argc, char **argv) {
-	/*int numthreads = omp_get_num_threads();*/
+
 	// Defaults with no params
-	int totalItter = 300 * 60; // total seconds to simulate TODO set this with cmd line arg as total minutes
-	int numberSats = 2; // some way of loading our satilite orbit params from a config-like file (e.g. json, txt) will be needed
-	/*
+    int numThreads = 2; // TODO set this with flag
+	int totalItter = 100 * 60; // total seconds to simulate
+	int numberSats = 2; // TODO some way of loading our satilite orbit params from a config-like file (e.g. json, txt) will be needed
+
+    /*
     #pragma omp parallel num_threads(5)
     {
         printf("Hello from %i of %i\n", omp_get_thread_num(), omp_get_num_threads());
     }
 	*/
-	for (int i = 1; i < argc; i++) {
+
+    for (int i = 1; i < argc; i++) {
 		if (strcmp(argv[i], "-sats") == 0 || strcmp(argv[i], "-s") == 0) {
-			// Set time in minutes
+			// Set number of satellites
 			char* numberSats = argv[i + 1];
 			printf("numberSats = %s\n", numberSats);
 		}
 		else if (strcmp(argv[i], "-time") == 0 || strcmp(argv[i], "-t") == 0) {
 			// Set time in minutes
 			char* totalIterStr = argv[i + 1];
-			int totalIterNum = atoi(totalIterStr) * 60;
-			printf("totalItter = %i\n ", totalIterNum);			
-		}	
+			totalItter = atoi(totalIterStr) * 60;
+			printf("totalItter = %i\n ", totalItter);
+		}
+        else if (strcmp(argv[i], "-help") == 0 || strcmp(argv[i], "-h") == 0) { // help string
+		    printf("\nUsage: ./main [-time t] [-sats s]\n\n");
+            return 0;
+		}
 	}
 
-	int curItter = 0;
+    /** Simulating */
 
 	satellite_t *satellites = (satellite_t*) malloc( numberSats * sizeof(satellite_t) );
 	init_satellites(satellites, numberSats);
 
-    printf("Size of sat_t: %i\n", sizeof(satellite_t));
+    printf("Size of sat_t: %lu\n", sizeof(satellite_t));
 
+    int curItter = 0;
 	for (int curItter = 0; curItter<totalItter; curItter++) {
 
 		// printf("%f\n", satellites[0].trueAnomaly);
